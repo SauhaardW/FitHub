9341ed56daed1aa69d2353ee70663b05294792e8
Andres <andrewpriority1@gmail.com>
2022-10-06 12:32:31 -0400

    [FIT-25] Initial login page creation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Display from './Display'
+import Display from './Display';
+import Login from './pages/Login';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Display />
+    {/* <App /> */}
+    {/* <Display /> */}
+    <Login />
   </React.StrictMode>
 );
