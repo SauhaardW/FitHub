40e0ecc2a7e6779a3e53935439655ec2f707938e
Andres <andrewpriority1@gmail.com>
2022-10-06 15:47:37 -0400

    [FIT-25] Created login frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import Display from './Display';
 import Login from './pages/Login';
 import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+<<<<<<< HEAD
     <App />
+=======
+    <Login />
+>>>>>>> 3b22860 ([FIT-25] Created login frontend)
   </React.StrictMode>
 );
