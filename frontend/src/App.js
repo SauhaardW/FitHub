2712d1103553c226292388f78a2b69d90b8126f2
Andres <andrewpriority1@gmail.com>
2022-10-06 22:26:10 -0400

    [FIT-25] Replaced wrong tag in Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopBar from './components/TopBar';
 import Profile from "./pages/Profile";
