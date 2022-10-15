a4aefc5afe316ed8175b128d86caa495f3493069
Andres <andrewpriority1@gmail.com>
2022-10-14 20:44:34 -0400

    Deploy server.js heroku client rename

--- server.js
+++ server.js
@@ -30,9 +30,9 @@ app.use("/", router)
 
 // Heroku deploy
 const path = require("path");
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "frontend", "build")))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 
