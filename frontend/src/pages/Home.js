ea003b25d6533ad4ea5aaf52dc0600a78de25b94
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2022-10-16 19:01:00 -0400

    [S2-13] layout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
         const myWorkouts = []
         const recWorkouts = []
 
-        const url = "http://localhost:3001/api/workouts"
+        const url = "http://localhost:3001/api/workouts?subset=true"
         axios.get(url).then(res => {
            const data = res.data.data;
             data.forEach(workout => {
@@ -47,92 +47,96 @@ const Home = () => {
 
 
     return (
-        <div className="pages mx-3 page-font">
-            <div className="text-4xl font-normal">
-                Hello, {name}!
+        <div className="pages mx-3 page-font flex flex-col justify-between">
+            <div>
+                <div className="text-4xl font-normal">
+                    Hello, {name}!
+                </div>
+
+                <hr className="mt-1 mb-5 h-px bg-black border-0"></hr>
+
+                {userWorkouts !== null && userWorkouts !== undefined && userWorkouts !== [] && <div>
+                    <div className="text-md m-1 font-semibold">
+                        YOUR WORKOUTS
+                    </div>
+
+                    <div className="horizontal-scrollable-div">
+                        <ul className="flex">
+                            {userWorkouts.map((workout) => {
+                                return (
+                                    //change outline colour
+                                    <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
+                                        <div
+                                            key={workout.name}
+                                            className="w-60 text-[#3898F2]"
+                                        >
+                                            <div className="font-bold text-lg">
+                                                {workout.name.toUpperCase()}
+                                            </div>
+                                            <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
+
+
+                                            <div className="text-xs">
+                                                {workout.exercisesString}
+                                            </div>
+
+                                            <div className="text-right font-semibold mt-2 text-sm text-black">
+                                                {workout.exercises_info.length} exercises
+                                            </div>
+
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>}
+
+                {recommendedWorkouts !== null && recommendedWorkouts !== undefined && recommendedWorkouts !== [] && <div>
+                    <div className="text-md m-1 mt-3 font-semibold">
+                        OUR WORKOUTS
+                    </div>
+
+                    <div className="horizontal-scrollable-div">
+                        <ul className="flex">
+                            { recommendedWorkouts.map((workout) => {
+                                return (
+                                    <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
+                                        <div
+                                            key={workout.name}
+                                            className="w-60 text-[#3898F2]"
+                                        >
+                                            <div className="font-bold text-lg">
+                                                {workout.name.toUpperCase()}
+                                            </div>
+                                            <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
+
+                                            <div className="text-xs">
+                                                {workout.exercisesString}
+                                            </div>
+
+                                            <div className="text-right font-semibold mt-2 text-sm text-black">
+                                                {workout.exercises_info.length} exercises
+                                            </div>
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>}
             </div>
 
-            <hr className="mt-1 mb-5 h-px bg-black border-0"></hr>
-
-            {userWorkouts !== null && userWorkouts !== undefined && userWorkouts !== [] && <div>
-                <div className="text-md m-1 font-semibold">
-                    YOUR WORKOUTS
-                </div>
-
-                <div className="horizontal-scrollable-div">
-                    <ul className="flex">
-                        {userWorkouts.map((workout) => {
-                            return (
-                                //change outline colour
-                                <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
-                                    <div
-                                        key={workout.name}
-                                        className="w-60 text-[#3898F2]"
-                                    >
-                                        <div className="font-bold text-lg">
-                                            {workout.name.toUpperCase()}
-                                        </div>
-                                        <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
-
-
-                                        <div className="text-xs">
-                                            {workout.exercisesString}
-                                        </div>
-
-                                        <div className="text-right font-semibold mt-2 text-sm text-black">
-                                            {workout.exercises_info.length} exercises
-                                        </div>
-
-                                    </div>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>}
-
-            {recommendedWorkouts !== null && recommendedWorkouts !== undefined && recommendedWorkouts !== [] && <div>
-                <div className="text-md m-1 mt-3 font-semibold">
-                    OUR WORKOUTS
-                </div>
-
-                <div className="horizontal-scrollable-div">
-                    <ul className="flex">
-                        { recommendedWorkouts.map((workout) => {
-                            return (
-                                <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
-                                    <div
-                                        key={workout.name}
-                                        className="w-60 text-[#3898F2]"
-                                    >
-                                        <div className="font-bold text-lg">
-                                            {workout.name.toUpperCase()}
-                                        </div>
-                                        <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
-
-                                        <div className="text-xs">
-                                            {workout.exercisesString}
-                                        </div>
-
-                                        <div className="text-right font-semibold mt-2 text-sm text-black">
-                                            {workout.exercises_info.length} exercises
-                                        </div>
-                                    </div>
-                                </li>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </div>}
-
-        <button
-            className="absolute bottom-10 bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded"
-            onClick={() => {
-                navigate("/workouts/create");
-            }}
-        >
-            {CreateWorkout}
-        </button>
+            <div className="text-center mt-14">
+                <button
+                    className="bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded"
+                    onClick={() => {
+                        navigate("/workouts/create");
+                    }}
+                >
+                    {CreateWorkout}
+                </button>
+            </div>
         </div>
     );
 }
