c952d5b454794fa55b6b92733041f9d274c08fbc
alinabuzila <88336627+alinabuzila@users.noreply.github.com>
2022-10-16 18:55:45 -0400

    [S2-13] rebase

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -11,7 +11,9 @@ const Home = () => {
     const [name, setName] = useState("");
 
     useEffect( () => {
-        // Anything in here is fired on component mount.
+        axios.get("http://localhost:3001/api/current-user").then(res => {
+            setName(res.data.data.name);
+        })
 
        // can not update a state in for loop because state updates asynch, so by next iteration the state may not be updated yet
         const myWorkouts = []
@@ -21,6 +23,16 @@ const Home = () => {
         axios.get(url).then(res => {
            const data = res.data.data;
             data.forEach(workout => {
+                let exercises = "";
+                workout.exercises_info.forEach((exercise, index) => {
+                    exercises += exercise.name
+                    if (index !== workout.exercises_info.length - 1){
+                        exercises += ", "
+                    }
+                })
+                workout.exercisesString = exercises;
+                console.log(workout)
+
                 if (workout.username === "FitHub"){
                     recWorkouts.push(workout)
                 }
@@ -36,35 +48,41 @@ const Home = () => {
 
     return (
         <div className="pages mx-3 page-font">
-            <div className="text-4xl font-normal mb-5 underline">
+            <div className="text-4xl font-normal">
                 Hello, {name}!
             </div>
+
+            <hr className="mt-1 mb-5 h-px bg-black border-0"></hr>
+
             {userWorkouts !== null && userWorkouts !== undefined && userWorkouts !== [] && <div>
-                <div className="text-lg font-semibold">
+                <div className="text-md m-1 font-semibold">
                     YOUR WORKOUTS
                 </div>
 
                 <div className="horizontal-scrollable-div">
                     <ul className="flex">
-                        {(userWorkouts === undefined || userWorkouts === null || userWorkouts.length === 0) ? <div key="None" className="text-black text-sm p-2">No friends to display!</div> : userWorkouts.map((workout) => {
+                        {userWorkouts.map((workout) => {
                             return (
                                 //change outline colour
-                                <li key={workout.name} className="flex w-52 justify-between p-5 m-1 outline outline-1 outline-blue-700 rounded-lg">
+                                <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
                                     <div
                                         key={workout.name}
-                                        // className="m-1 p-4 bg-gray-200 rounded-md flex justify-between items-center shadow-md md:filter-none"
-                                        className="w-52"
+                                        className="w-60 text-[#3898F2]"
                                     >
-                                        <div className="font-bold text-sm">
-                                            {workout.name}
+                                        <div className="font-bold text-lg">
+                                            {workout.name.toUpperCase()}
+                                        </div>
+                                        <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
+
+
+                                        <div className="text-xs">
+                                            {workout.exercisesString}
                                         </div>
 
-                                        {/*<button*/}
-                                        {/*    className="px-2 py-1 bg-default-gradient rounded-lg w-30 text-white text-sm"*/}
-                                        {/*    // onClick={onScheduleClicked}*/}
-                                        {/*>*/}
-                                        {/*    /!*{schedule}*!/*/}
-                                        {/*</button>*/}
+                                        <div className="text-right font-semibold mt-2 text-sm text-black">
+                                            {workout.exercises_info.length} exercises
+                                        </div>
+
                                     </div>
                                 </li>
                             );
@@ -73,15 +91,48 @@ const Home = () => {
                 </div>
             </div>}
 
+            {recommendedWorkouts !== null && recommendedWorkouts !== undefined && recommendedWorkouts !== [] && <div>
+                <div className="text-md m-1 mt-3 font-semibold">
+                    OUR WORKOUTS
+                </div>
 
-            <button
-                className="absolute bottom-10 bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded"
-                onClick={() => {
-                    navigate("/workouts/create");
-                }}
-            >
-                {CreateWorkout}
-            </button>
+                <div className="horizontal-scrollable-div">
+                    <ul className="flex">
+                        { recommendedWorkouts.map((workout) => {
+                            return (
+                                <li key={workout.name} className="flex w-60 justify-between p-3 m-1 mr-3 outline outline-1 outline-[#3898F2] rounded">
+                                    <div
+                                        key={workout.name}
+                                        className="w-60 text-[#3898F2]"
+                                    >
+                                        <div className="font-bold text-lg">
+                                            {workout.name.toUpperCase()}
+                                        </div>
+                                        <hr className="mt-1 mb-3 h-px bg-[#3898F2] border-0"></hr>
+
+                                        <div className="text-xs">
+                                            {workout.exercisesString}
+                                        </div>
+
+                                        <div className="text-right font-semibold mt-2 text-sm text-black">
+                                            {workout.exercises_info.length} exercises
+                                        </div>
+                                    </div>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>}
+
+        <button
+            className="absolute bottom-10 bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded"
+            onClick={() => {
+                navigate("/workouts/create");
+            }}
+        >
+            {CreateWorkout}
+        </button>
         </div>
     );
 }
