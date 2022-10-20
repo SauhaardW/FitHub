1dd8fc493db8965e35eb21c7d7a49f2fd1cb1803
Manas1818 <79381912+Manas1818@users.noreply.github.com>
2022-10-20 00:13:00 -0400

    [S2-15] BUG FIXES

--- frontend/src/components/WorkoutComponent.js
+++ frontend/src/components/WorkoutComponent.js
@@ -20,13 +20,13 @@ const WorkoutComponent = (props) => {
         </div>
         {isActive && <div className="p-6 accordion-content">
               {/* <img src={props.exercise.gif} onError="this.onerror=null; this.src='https://www.planetfitness.com/sites/default/files/feature-image/xbreak-workout_602724.jpg.pagespeed.ic.v8byD7su-e.jpg';" /> */}
-               <div> <span className='font-medium text-blue-600'>Mechanics: </span>{exercise.classification.mechanics}<br></br></div>
-               <div> <span className='font-medium text-blue-600'>Force: </span>{exercise.classification.force}<br></br></div>
-               <div> <span className='font-medium text-blue-600'>Utility:</span> {exercise.classification.utility}<br></br></div>
-              <div>  <span className='font-medium text-blue-600'>Target Muscle:</span> {exercise.muscles.target}<br></br> </div>
-               <div> <span className='font-medium text-blue-600'>Preparation:</span> {exercise.instructions.preparation}<br></br></div>
-               <div> <span className='font-medium text-blue-600'>Execution: </span>{exercise.instructions.execution}<br></br></div>
-               <div> <span className='font-medium text-blue-600'>Comments: </span>{exercise.comments}<br></br></div>
+               <div> <span className='font-medium text-blue-600'>Mechanics: </span>{props.exercise.classification.mechanics}<br></br></div>
+               <div> <span className='font-medium text-blue-600'>Force: </span>{props.exercise.classification.force}<br></br></div>
+               <div> <span className='font-medium text-blue-600'>Utility:</span> {props.exercise.classification.utility}<br></br></div>
+              <div>  <span className='font-medium text-blue-600'>Target Muscle:</span> {props.exercise.muscles.target}<br></br> </div>
+               <div> <span className='font-medium text-blue-600'>Preparation:</span> {props.exercise.instructions.preparation}<br></br></div>
+               <div> <span className='font-medium text-blue-600'>Execution: </span>{props.exercise.instructions.execution}<br></br></div>
+               <div> <span className='font-medium text-blue-600'>Comments: </span>{props.exercise.comments}<br></br></div>
                <button className='bg-default-gradient mt-2 hover:bg-blue-700 ml-24 place-content-center px-12 py-4 text-white font-bold px-8 rounded mx-2'
                     onClick={()=>{
                         change_list(props.exercise._id)
