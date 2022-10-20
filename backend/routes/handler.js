6e18be553957bd0e5ea6ff3604056e21aeb9a10c
mojohosseinii <hosseini.sayedmojtaba@gmail.com>
2022-10-20 15:38:34 -0400

    [S2-22] - Changed API endpoint name

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -29,7 +29,7 @@ router.get("/api/get-friend-requests", friend_request.get_friendRequests);
 router.post("/api/workouts", workout.post);
 router.get("/api/workouts", workout.getUserWorkouts);
 
-router.post("/api/schedule", schedule.scheduleWorkouts);
-router.get("/api/schedule", schedule.getScheduledWorkouts);
+router.post("/api/schedule-workout", schedule.scheduleWorkouts);
+router.get("/api/get-scheduled-workouts", schedule.getScheduledWorkouts);
 
 module.exports = router;
