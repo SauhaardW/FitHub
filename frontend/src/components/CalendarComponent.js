d691153bd950b08871bca05cfd28ae70dcc2e857
SauhaardW <Sauhaard@gmail.com>
2022-10-21 14:34:13 -0400

    [S2-34] - Changed a href="#" to href="/#" to get rid of an warning message

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -201,7 +201,7 @@ function Workout({ workout }) {
               <Menu.Item>
                 {({ active }) => (
                   <a
-                    href=""
+                    href="/#"
                     className={classNames(
                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                       'block px-4 py-2 text-sm'
