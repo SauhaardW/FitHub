0e44266735c546e9b1388717b03f62a453a3acaa
buzila <buzila@Andreeas-Air.local>
2022-10-21 20:01:35 -0400

    [S2-13] minor styling changes

--- frontend/src/pages/Calendar.js
+++ frontend/src/pages/Calendar.js
@@ -22,7 +22,7 @@ const Calendar = () => {
 
       <CalendarComponent></CalendarComponent>
       <button
-        className="absolute bottom-10 bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded-xl text-xl"
+        className="absolute bottom-10 bg-default-gradient text-white py-4 px-10 w-3/4 left-[calc(12.5vw)] rounded text-xl"
         onClick={() => {
           navigate("/calendar/schedule-workout");
         }}
